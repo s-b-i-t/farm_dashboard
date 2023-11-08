c3e6ed0eb81db9f29ba233514480bf9abb2a697b
Shabit Bhatt <bhattshabit@gmail.com>
2023-11-08 14:24:19 -0500

    station info

--- src/utility.js
+++ src/utility.js
@@ -55,18 +55,18 @@ export function DisplayData({ weatherData }) {
 export function DisplayStationInfo({stationInfo}) {
   return (
     <div>
-      <p>Station ID: {stationInfo.stationID}</p>
-      <p>Time Zone: {stationInfo.tz}</p>
-      <p>Observation Time (UTC): {stationInfo.obsTimeUtc}</p>
-      <p>Observation Time (Local): {stationInfo.obsTimeLocal}</p>
-      <p>Latitude: {stationInfo.lat}</p>
-      <p>Longitude: {stationInfo.lon}</p>
-      <p>Solar Radiation High: {stationInfo.solarRadiationHigh}</p>
-      <p>UV High: {stationInfo.uvHigh}</p>
-      <p>Average Wind Direction: {stationInfo.winddirAvg}°</p>
-      <p>High Humidity: {stationInfo.humidityHigh}%</p>
-      <p>Low Humidity: {stationInfo.humidityLow}%</p>
-      <p>Average Humidity: {stationInfo.humidityAvg}%</p>
+      { <p>Station ID: {stationInfo?.stationID}</p> }
+      { <p>Time Zone: {stationInfo?.tz}</p> }
+      { <p>Observation Time (UTC): {stationInfo?.obsTimeUtc}</p> }
+      { <p>Observation Time (Local): {stationInfo?.obsTimeLocal}</p> }
+      { <p>Latitude: {stationInfo?.lat}</p> }
+      { <p>Longitude: {stationInfo?.lon}</p> }
+      { <p>Solar Radiation High: {stationInfo?.solarRadiationHigh}</p> }
+      { <p>UV High: {stationInfo?.uvHigh}</p>  }
+       { <p>Average Wind Direction: {stationInfo?.winddirAvg}°</p> }
+      { <p>High Humidity: {stationInfo?.humidityHigh}</p> }
+      { <p>Low Humidity: {stationInfo?.humidityLow}</p> }
+      { <p>Average Humidity: {stationInfo?.humidityAvg}</p>  }
     </div>
   );
 }
