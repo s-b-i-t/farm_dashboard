51c5a080efb62a6f9c143a487deb508a34d6145f
Danny Baker <113060653+chefbaker4@users.noreply.github.com>
2024-03-09 18:43:33 -0500

    added nn predictions

--- flask/app.py
+++ flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import pickle
 import pandas as pd
-
+from datetime import datetime
 app = Flask(__name__)
 CORS(app)
 
@@ -32,7 +32,6 @@ def predict():
     prec = request_data.get('prec', 0)
     slrt = request_data.get('slrt', 0)
     wspd = request_data.get('wspd', 0)
-
     # Prepare data for prediction
     new_df = pd.DataFrame({'airt': [airt], 'prec': [prec], 'slrt': [slrt], 'wspd': [wspd]})
 
@@ -45,19 +44,37 @@ def predict():
     # Neural Network Model Prediction
     nn_predictions = NNModel_Daily.predict(new_df)
     soil_temp_nn = nn_predictions[0]
+    
+    current_month = datetime.now().month
 
-    predictions = {
-        'warm_season': {
-            'air_temperature': airt,
-            'soil_temperature_lr': float(soil_temp_warm),
-            'soil_temperature_nn': float(soil_temp_nn)
-        },
-        'cold_season': {
-            'air_temperature': airt,
-            'soil_temperature_lr': float(soil_temp_cold),
-            'soil_temperature_nn': float(soil_temp_nn)
+    if current_month in [1,2,3,10,11,12]:
+        predictions = {
+            'season': {
+                'season_type': 'Cold',
+                'air_temperature': int(airt),
+                'soil_temperature_lr': int(soil_temp_cold)
+            
+            },
+            'nn_prediction': {
+                'air_temperature': int(airt),
+                'soil_temperature_nn': int(soil_temp_nn)
+            }
         }
-    }
+    elif current_month in [4,5,6,7,8,9]:
+        predictions = {
+            'season': {
+                'season_type': 'Warm',
+                'air_temperature': int(airt),
+                'soil_temperature_lr': int(soil_temp_warm)
+            
+            },
+            'nn_prediction': {
+                'air_temperature': int(airt),
+                'soil_temperature_nn': int(soil_temp_nn)
+            }
+        }
+    else:
+        print('\n\nWOAH\n\n\n\n\nnAHHHHHHHHHHHHHHHHHH\n\n\n')
     
     return jsonify(predictions)
 
